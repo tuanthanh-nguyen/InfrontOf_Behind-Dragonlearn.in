67d099047002601375a1645ff01f2148873377a9
Tuan Thanh <atonggiang0410@gmail.com>
2020-04-15 17:58:31 +0700

    add sound when click feature #8

--- Scene1.js
+++ Scene1.js
@@ -1,11 +1,10 @@
-
 class Scene1 extends Phaser.Scene{
     constructor(){
         super("game1");
-        this.drag_X = 400;
+        this.drag_X = 1200;
         this.drag_Y = 500;
-        this.drop_X = 700;
-        this.drop_Y = 300;
+        this.drop_X = 300;
+        this.drop_Y = 200;
         this.check;
         this.changeScene;
     }
@@ -13,27 +12,48 @@ class Scene1 extends Phaser.Scene{
         this.load.image('next','image/back_icon.png');
         this.load.image('drag', 'image/35.png');
         this.load.image('drop', 'image/53.png');
+        this.load.image('sound', 'image/loa.png');
+        this.load.image('bag', 'image/41.png');
+        this.load.audio('sfx','voice.mp3');
     }
     create(){
         this.add.text(20,20,"Loading game1...");
+        // this.cameras.main.setBackgroundColor(0xDDDDD);
+        this.voice = this.sound.add("sfx");
+        // this.voice.play();
+        //create next arrow but set it invisible and turn around
+        this.sprite = this.add.sprite(700, 700, 'next').setInteractive();
+        this.sprite.setVisible(false);
+        this.sprite.angle = 180;
 
         //create and display items to canvas
-        this.sprite = this.add.sprite(700, 500, 'next').setInteractive();
-        this.sprite.setScale(0.2);
-        this.sprite.setVisible(false);
-        //declare a this reference for convinient
-        var ref = this;
-        this.sprite.angle = 180;
-        
-        this.drop = this.physics.add.sprite(this.drop_X ,this.drop_Y,'drop');
+        this.drop = this.physics.add.sprite(this.drop_X ,this.drop_Y,'drop').setInteractive();
+        this.sound = this.physics.add.sprite(this.drag_X ,this.drag_Y,'sound').setInteractive();
+        this.bag = this.physics.add.sprite(this.drag_X ,this.drag_Y,'bag').setInteractive();
         this.drag = this.physics.add.sprite(this.drag_X ,this.drag_Y,'drag').setInteractive();
+
+        //scale items in canvas
+        this.sprite.setScale(0.5);
+        this.drop.setScale(1.5);
+        this.drag.setScale(1.5);
+        this.bag.setScale(1.5);
 
         //set physics of the ball
         this.drag.setCollideWorldBounds(true);
 
         //allow items to be dragged
         this.input.setDraggable(this.drag);
+        this.input.setDraggable(this.drop);
+        this.input.setDraggable(this.sound);
+        this.input.setDraggable(this.bag);
 
+
+        
+        
+
+        this.handler();
+    }
+    handler(){
         //invoke when start to drag
         this.input.on('dragstart', function (pointer,gameObject) {});
 
@@ -43,22 +63,32 @@ class Scene1 extends Phaser.Scene{
             gameObject.y = dragY;
         });
 
-        //invoke when release the dragging items
-        this.input.on('dragend', function () {
-            //create animation to return to original pos if invalid drop
-            this.animation(this,this.drag_X,this.drag_Y);
+        //declare a this reference for convinient
+        var ref = this;
 
-            //overlapping process: ****NEED TO MODIFY THIS SHIT RIGHT AWAY****
-            var ref = this;
-            setTimeout(function(){ref.check = ref.physics.add.overlap(ref.drag,ref.drop,ref.callback,null,ref)},0);
-        },ref);
-     
+        //invoke when release the dragging items
+        // this.input.on('dragend', function () {
+        //     //create animation to return to original pos if invalid drop
+        //     this.animation(this,this.drag_X,this.drag_Y);
+        //     console.log("drag end");
+        //     //overlapping process: ****NEED TO MODIFY THIS SHIT RIGHT AWAY****
+        //     var ref = this;
+        //     ref.check = ref.physics.add.overlap(ref.drag,ref.drop,ref.callback,null,ref)
+        // },ref);
+
+        //invoke when finish the valid pos
         this.sprite.on('pointerdown', function (pointer) {
             console.log("click");
             console.log(ref);
-            ref.scene.start("game2");
-            
+            ref.scene.start("game2");    
         });
+
+        this.sound.on('pointerdown', function (pointer) {
+            console.log("click");
+            console.log(ref);
+            ref.voice.play();   
+        });
+        
     }
     animation(ref,pos_x,pos_y){
         var timeline = ref.tweens.createTimeline();
@@ -74,9 +104,6 @@ class Scene1 extends Phaser.Scene{
     callback(){
         //set object to unDraggable
         this.input.setDraggable(this.drag,false);
-
-        //deactivate drop area so it cant be multiple time overlapping
-        this.check.active = false;
 
         //create animation to place to exact pos
         this.animation(this,this.drop_X+100,this.drop_Y);
